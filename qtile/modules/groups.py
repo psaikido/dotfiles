31093f87ffbed70b84952aa2a0374f4647b4c419
Hugh Carroll <hughie@carrollonline.co.uk>
2023-10-26 12:06:49 +0100

    Different wallpaper per group.

--- qtile/modules/groups.py
+++ qtile/modules/groups.py
@@ -1,8 +1,26 @@
+from libqtile import qtile, hook
 from libqtile.config import Key, Group
 from libqtile.command import lazy
 from .keys import keys, mod
+from libqtile.utils import send_notification
+
 
 groups = [Group(i) for i in "123456789"]
+# wall_path = '/home/hughie/Pictures/wallpapers/theme-hypr/'
+wall_path = '/home/hughie/Pictures/wallpapers/theme-i3/'
+wallpapers = [
+    wall_path + '1.jpg',
+    wall_path + '2.jpg',
+    wall_path + '3.jpg',
+    wall_path + '4.jpg',
+    wall_path + '5.jpg',
+    wall_path + '6.jpg',
+    wall_path + '7.jpg',
+    wall_path + '8.jpg',
+    wall_path + '9.jpg',
+    wall_path + '10.jpg',
+]
+
 
 for i in groups:
     keys.extend([
@@ -18,13 +36,17 @@ for i in groups:
         Key([mod], "Left", lazy.screen.prev_group(),
             desc="Switch to previous group"),
 
-        # mod1 + shift + letter of group = switch to & move focused window to group
+        # mod1 + shift + letter of group = move focused window to group
         Key([mod, "shift"],
             i.name,
             lazy.window.togroup(i.name, switch_group=False),
             desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
     ])
+
+
+@hook.subscribe.setgroup
+def screen_change():
+    group_index = qtile.groups.index(qtile.current_group)
+    # send_notification("qtile", groups[group_index].name)
+    wallpaper = wallpapers[group_index]
+    qtile.paint_screen(qtile.current_screen, wallpaper, mode="fill")
--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -44,6 +44,7 @@ screens = [
                     },
                     background="#2f343f"),
                 widget.Systray(icon_size = 20),
+                widget.Notify(),
                 widget.BatteryIcon(),
                 widget.Battery(format="{char} {percent:2.0%} {hour:d}:{min:02d}"),
                 widget.Wallpaper(label="| wall"),
